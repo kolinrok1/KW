933d66bd3d7a715b927e345e611c5ec3b94aedbe
Alex.Emelianov <relife35@yandex.ru>
2022-09-18 19:41:49 +0300

    финал

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿void ArrRandom (string [] array)
+﻿void ArrFill (string [] array)
 {
     for ( int i=0; i<array.Length; i++)
     {
@@ -20,16 +20,19 @@ Console.WriteLine("Введите длинну массива");
 int size =Convert.ToInt32(Console.ReadLine());
 string [] array = new string[size];
 int count = 0;
-ArrRandom(array);
+ArrFill(array);
+Console.WriteLine();
+Console.Write("Ваш массив: ");
 PrintArray(array);
-string [] array2 = new string [size];
+string [] arraySecond = new string [size];
 for (int i=0 ; i<array.Length;i++) 
 {
     if (array[i].Length<= 3) 
     {
-        array2[count]= array[i];
+        arraySecond[count]= array[i];
         count++;
     }
 }
-PrintArray(array2);
-//string [] secondarray = new string[count];
+Console.WriteLine();
+Console.Write("Итоговый массив: ");
+PrintArray(arraySecond);
