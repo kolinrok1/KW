2d101ca2c8577dbbc1ab78e2699b0c1845fe515a
Alex.Emelianov <relife35@yandex.ru>
2022-09-18 19:20:06 +0300

    second

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ArrRandom (string [] array)
+{
+    for ( int i=0; i<array.Length; i++)
+    {
+        Console.Write("Введите элемент "+ i +" :");
+        array [i]= Console.ReadLine();
+    }
+}
+
+void PrintArray (string [] array)
+{
+    for (int i=0; i<array.Length;i++)
+    {
+    Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine(" ");
+}
+
+Console.WriteLine("Введите длинну массива");
+int size =Convert.ToInt32(Console.ReadLine());
+string [] array = new string[size];
+ArrRandom(array);
+PrintArray(array);
