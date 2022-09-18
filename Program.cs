29be3efdcd853b80ff6e75f9ed441580aabc8cc4
Alex.Emelianov <relife35@yandex.ru>
2022-09-18 19:28:13 +0300

    счет числе

--- Program.cs
+++ Program.cs
@@ -19,5 +19,11 @@ void PrintArray (string [] array)
 Console.WriteLine("Введите длинну массива");
 int size =Convert.ToInt32(Console.ReadLine());
 string [] array = new string[size];
+int count = 0;
 ArrRandom(array);
 PrintArray(array);
+for (int i=0 ; i<array.Length;i++) 
+{
+    if (array[i].Length<= 3) {count ++;}
+}
+Console.WriteLine(count);
